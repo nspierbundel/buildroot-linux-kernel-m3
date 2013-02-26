cd5376af715a3dc95bc79e6e0ee496d7983b1134
nspierbundel <nspierbundel@hotmail.com>
2013-02-26 10:03:31 +0000

    change to my build env

--- env.sh
+++ env.sh
@@ -1,12 +1,15 @@
 export ARCH=arm
-export CROSS_COMPILE=/root/buildroot-AmLogic/output/external-toolchain/bin/arm-none-linux-gnueabi-
-#export CROSS_COMPILE=arm-none-linux-gnueabi-
+export CROSS_COMPILE=/data/buildroot-AmLogic/output/external-toolchain/bin/arm-none-linux-gnueabi-
+#export CROSS_COMPILE=arm-linux-gnueabihf-
+#export LIBRARY_PATH=/usr/lib/gcc/arm-linux-gnueabihf/4.4.6/
 make clean
-make meson_reff16_gbox_defconfig
-make menuconfig
+make meson_reff16_bluem3_defconfig
+#make menuconfig
 #make vmlinux
 
 TOP=${PWD}
 export MKIMAGE=${TOP}/arch/arm/boot/mkimage
 make uImage
 #make modules
+cp arch/arm/boot/uImage /data/buildroot-AmLogic/output/images/uImage-2.6.34
+
